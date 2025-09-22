a510e1ec8720aea52018d2171b27c9a7415e7d8d
cotiaws <cotiaws5@gmail.com>
2025-09-21 23:57:01 -0300

    teste

--- src/test/java/cotiinformatica/runners/Runner.java
+++ src/test/java/cotiinformatica/runners/Runner.java
@@ -1,16 +1,19 @@
 package cotiinformatica.runners;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/java/cotiinformatica/features", //local das features
-		glue = "cotiinformatica/tests", //pacote onde estão as classes de testes
-		plugin = { "pretty", "html:target/cucumber-report.html" } //relatórios
-		)
+    features = "src/test/java/cotiinformatica/features",
+    glue = "cotiinformatica/tests",
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber-report.html",
+        "json:target/cucumber-reports/cucumber-report.json",
+        "junit:target/cucumber-reports/cucumber-report.xml"
+    }
+)
 public class Runner {
-
 }
